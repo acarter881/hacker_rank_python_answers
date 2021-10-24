8c0a940d937e4fda29719e3f3ae825c3987b3887
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-10-24 15:41:03 -0500

    Update mod_divmod.py

--- mod_divmod.py
+++ mod_divmod.py
@@ -1,8 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys
 
-s = [line.rstrip() for line in sys.stdin.readlines()]
+s = [int(line.rstrip()) for line in sys.stdin.readlines()]
 
-print(int(s[0]) // int(s[1]))           # Integer division
-print(int(s[0]) % int(s[1]))            # Modulo
-print(divmod(int(s[0]), int(s[1])))     # divmod
+print(s[0] // s[1])           # Integer division
+print(s[0] % s[1])            # Modulo
+print(divmod(s[0], s[1]))     # divmod
