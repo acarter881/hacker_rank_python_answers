a62d2f3896c8031d36e699bc147d63d1debec340
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-10-24 18:18:04 -0500

    Create set_intersection.py

---
+++ set_intersection.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+
+my_list = []
+
+for index, line in enumerate(sys.stdin.read().split('\n')):
+    if index % 2 != 0:
+        my_list.append(([value for value in line.split()]))     
+           
+l1 = set(my_list[0])
+l2 = set(my_list[1]) 
+
+print(len(l1.intersection(l2)))
