48a16e27ea5ce9364556f3aa6f96cf42088ca194
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-10-24 15:29:11 -0500

    Update zipped.py

--- zipped.py
+++ zipped.py
@@ -7,7 +7,7 @@ for line in sys.stdin.readlines()[1:]:
     line = line.strip()
     my_list.append(map(float, line.split()))
     
-s = list(zip(*my_list))
+s = list(zip(*my_list)) # unpacking
 
 for avg in [sum(s[i]) / len(s[i]) for i in range(len(s))]:
     print(avg)
