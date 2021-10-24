da07bc98210e52c4fac0451ff6cef1797b0e3644
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-10-24 15:24:05 -0500

    Create zipped.py

---
+++ zipped.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+
+my_list = []
+
+for line in sys.stdin.readlines()[1:]:
+    line = line.strip()
+    my_list.append(map(float, line.split()))
+    
+s = list(zip(*my_list))
+
+for avg in [sum(s[i]) / len(s[i]) for i in range(len(s))]:
+    print(avg)
