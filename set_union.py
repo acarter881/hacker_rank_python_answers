b6840d47df296ff9e8378816b4df4c3b9374f556
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-10-24 14:31:47 -0500

    Update set_union.py

--- set_union.py
+++ set_union.py
@@ -1,11 +1,10 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys
 
 my_list = []
 
 for index, line in enumerate(sys.stdin.read().split('\n')):
     if index % 2 != 0:
-        my_list.append(' '.join(([value for value in line.split() if value != ' '])))
+        my_list.append(' '.join(([value for value in line.split()])))
 
 my_list = my_list[0] + ' ' + my_list[1]   
 
