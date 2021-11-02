62cb677fbf59d8974e759559146c7c9416d754ce
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-11-01 23:35:45 -0500

    Create set_discard_remove_pop.py

---
+++ set_discard_remove_pop.py
@@ -0,0 +1,15 @@
+# TIP: Use Python3 instead of Pypy3
+
+import sys
+
+n = int(input())
+s = set(map(int, input().split()))
+
+data = sys.stdin.read().split('\n')
+
+for i in range(1, len(data), 1):
+    if data[i].split()[0] == 'pop': s.pop()
+    elif data[i].split()[0] == 'remove': s.remove(int(data[i].split()[1]))
+    elif data[i].split()[0] == 'discard': s.discard(int(data[i].split()[1]))
+
+print(sum(s))
